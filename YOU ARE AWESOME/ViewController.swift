64613ea07a1ed06c10ddb52cbdf55f39cbfebfed
Mark <mark@Marks-MacBook-Pro.local>
2018-01-22 17:19:51 -0500

    Commit for hw

--- YOU ARE AWESOME/ViewController.swift
+++ YOU ARE AWESOME/ViewController.swift
@@ -9,12 +9,29 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var messageLabel: UITextView!
+    
+    var index = 0
+    
+    let messages = ["You Are Fantastic!", "You Are Great!", "You Are Amazing!", "When the Genius Bar needs help, they call you!", "You Brighten My Day!", "You Are Da Bomb!", "I can't wait to use your app!"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func buttonDidTapped(_ sender: Any) {
+        
+        messageLabel.text = messages[index]
+        index = index + 1
+        
+        if (index == messages.count) {
+            index = 0
+        }
+        
+        
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
